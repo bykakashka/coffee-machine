e11745d84534d3707a466fcc515a8169c77cecd3
bykakashka <belka4ever1102@gmail.com>
2016-07-20 15:42:15 +0300

    test

--- Coffee_Machine/Controllers/HomeController.cs
+++ Coffee_Machine/Controllers/HomeController.cs
@@ -5,8 +5,11 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
 
+// TODO add google
+
 namespace Coffee_Machine.Controllers
 {
+    [RequireHttps]
     public class HomeController : Controller
     {
         private DataBaseContext db = new DataBaseContext();
@@ -15,13 +18,11 @@ namespace Coffee_Machine.Controllers
             string name = (string) Session ["name"];
             if (name == null)
                 return View(new UserAndTodayCost ());
-
-            var p = new CostHistory ();
-            p.EndDate = null;
+            
             return View(new UserAndTodayCost{ 
-                 User = db.Users.Where(c => c.Login == name).FirstOrDefault(),
-                 Cost = db.History.Where(c => ( (c.EndDate == null) || (c.BeginDate <= DateTime.Now && c.EndDate > DateTime.Now) ) ).FirstOrDefault().Cost                   
-            });
+                User = db.Users.Where(c => c.Login == name).FirstOrDefault(),
+                Cost = db.History.Where(c => ( (c.EndDate == null) || (c.BeginDate <= DateTime.Now && c.EndDate > DateTime.Now) ) ).FirstOrDefault().Cost                   
+            });        
         }
     }
 }
